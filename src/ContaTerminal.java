421a5a9f8efb35b6bc90e065cdf657fd5cdad1c8
ANDERSONROCHA81 <ironmanrocha81@gmail.com>
2023-01-02 20:50:09 -0300

    Implementação da classe ContaTerminal

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
 
+        System.out.println("Por favor, informe o seu nome!");
+        String nomeCliente = leitor.nextLine();
+
+        System.out.println("Por favor, informe o número da agência!");
+        int agencia = leitor.nextInt();
+
+        System.out.println("Por favor, informe o número da conta!");
+        int conta = leitor.nextInt();
+
+        System.out.println("Por favor, informe o seu saldo!");
+        double saldo = leitor.nextDouble();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %d, conta %d e seu saldo de R$ %.2f já está disponível para saque.", nomeCliente, agencia, conta, saldo);
     }
 }
